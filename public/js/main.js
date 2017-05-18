ca39c5badc6d22f86c493821d18fe2a5bd69f0cd
Viktor Matushevskyi <viktorminator@gmail.com>
2017-05-18 15:01:00 +0300

    stamp rotation start

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
         ,hat = document.getElementById('hat')
         ,paper = document.getElementById('paper')
         ,stamp = document.getElementById('stamp')
+        ,swatch = document.getElementById('swatch')
         ,tail1 = document.getElementById('tail1')
         ,tail2 = document.getElementById('tail2')
         ,gen_text = document.getElementById('gen_text');
@@ -20,7 +21,7 @@ $(document).ready(function(){
 
     var tla = new TimelineMax();
     // About timeline animation
-    tla.from('.project__bcg', 3, { overflow: 'hidden', opacity: '0', backgroundSize: 'inherit', ease: Power0.easeIn, delay: -1 });
+   // tla.from('.project__bcg', 3, { overflow: 'hidden', opacity: '0', backgroundSize: 'inherit', ease: Power0.easeIn, delay: -1 });
     tla.from('#about_line', 1, { drawSVG: '0', delay: 2, ease: Power0.easeIn});
     tla.from('#about_text', 1.5, { x: '170', ease: Bounce.easeOut });
     tla.from('#about_mask', 1.5, { x: '-170', ease: Bounce.easeOut, delay: -1.5});
@@ -36,28 +37,38 @@ function startTween() {
     Gtl.from(tubebad, 3, { x: 0, y: -500 , ease:Elastic.easeOut.config(1, 0.75)});
     Gtl.to(tubebad, 1, { rotation: '-5', transformOrigin: '100% 0' }, 0.5);
     Gtl.to(tubebad, 1, { rotation: 0, transformOrigin: '100% 0' }, 1.2);
-    //Gtl.to('#tube', 0.1, { attr:{src:'/img/tube.png'}});
-    Gtl.to(tube, 0.1, { autoAlpha: 1 });
+    Gtl.to(swatch, 0.01, { autoAlpha: 1});
+    Gtl.to(tube, 0.1, { width: '+=6', attr:{src:'/img/tube.png'}});
     Gtl.to(hat, 0.1, { autoAlpha: 1 });
+    Gtl.to(tube, 0.1, { autoAlpha: 1 });
     Gtl.to(tubebad, 0.1, { autoAlpha: 0 });
     Gtl.to(paper, 0.1, { autoAlpha: 1 });
     Gtl.to(stamp, 0.1, { autoAlpha: 1 });
     
     /* TUBE OPENING */
-    Gtl.to(hat, 0.5, { x: '+=5', y: '+=5', rotation:-160, transformOrigin:"0px 0px"});
+    Gtl.to(swatch, 0.2, { x: '+=4', y: '+=2', rotation: 100, transformOrigin: "0 0", ease: Back.easeOut.config(4)});
+    Gtl.to(hat, 0.5, { x: '-=1', y: '+=5', rotation:-160, transformOrigin:"0px 0px"});
     Gtl.to(hat, 0.01, { x: '+=5', attr:{src:'/img/hat2.png'}});
+
     /* TUBE LEFT */
     Gtl.to(tube, 1, { x: -668, y: -10, scale: 1.25 });
-    Gtl.to(paper, 1, { x: -50, y: -10, delay: -1, scale: 1.2 });
-    Gtl.to(hat, 1, { x: -573, y: -15, delay: -1, scale: 1.25 });
+    Gtl.to(paper, 1, { x: -50, y: '-=8', delay: -1, scale: 1.2 });
+    Gtl.to(hat, 1, { x: -582, y: -13, delay: -1, scale: 1.25 });
+    Gtl.to(swatch, 1, { x: '-=593', y: '+=4', delay: -1, scale: 1.2 });
+    Gtl.to(swatch, 1, { rotation: '-=30', ease: Back.easeOut.config(4) }, 3.8);
+    Gtl.to(swatch, 0.5, { rotation: '+=10', ease: Back.easeOut.config(4) }, 4.4);
+    Gtl.to(tail1, 0.1, { autoAlpha: 1 }, 4.5);
+    Gtl.to(tail2, 0.1, { autoAlpha: 1 }, 4.5);
+
 
 // {bezier:[{left:100, top:250}, {left:300, top:0}, {left:500, top:400}], ease:Power1.easeInOut});
     Gtl.to(stamp, 0.1, {x: '-=20', y: '+=20',scale: 1.4});
-    Gtl.to(tail1, 0.1, {x: '+=50'}, 4.8);
+    Gtl.to(tail1, 0.05, {x: '+=50'}, 5.6);
     Gtl.to(stamp, 0.1, {x: '+=70', y: 60, transformOrigin:"50% 0" ,  bezier: [{x:0, y:0}, {x:50,y:10},{x:-10,y:100}]});
 
     Gtl.to(stamp, 1, { x: 95, rotation: '+=90',  ease:Elastic.easeOut});
-    Gtl.to(tail1, 1, { rotation: '+=120', bezier: [{x:0, y:0}, {x:40,y:40},{x:100,y:80}]}, 5.4);
+    Gtl.to(tail1, 1, { rotation: '+=120', bezier: [{x:0, y:0}, {x:40,y:40},{x:90,y:80}]}, 5.65);
+    Gtl.to(tail2, 1, { rotation: '+=120', bezier: [{x:0, y:0}, {x:40,y:40},{x:90,y:80}]}, 5.65);
     Gtl.to(gen_text, 1, { autoAlpha: 1 });
     Gtl.restart();
 }
